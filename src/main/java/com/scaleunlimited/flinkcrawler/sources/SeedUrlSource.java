ad997cf66570d3ebc37ea8e31e97188dceae8f02
Ken Krugler <ken@transpac.com>
2016-10-24 16:53:28 -0700

    Properly parallelize seed URLs

--- src/main/java/com/scaleunlimited/flinkcrawler/sources/SeedUrlSource.java
+++ src/main/java/com/scaleunlimited/flinkcrawler/sources/SeedUrlSource.java
@@ -1,30 +1,56 @@
 package com.scaleunlimited.flinkcrawler.sources;
 
-import java.util.Arrays;
 import java.util.LinkedList;
 
-import org.apache.flink.streaming.api.functions.source.ParallelSourceFunction;
+import org.apache.flink.api.common.functions.RuntimeContext;
+import org.apache.flink.configuration.Configuration;
+import org.apache.flink.streaming.api.functions.source.RichParallelSourceFunction;
 
 import com.scaleunlimited.flinkcrawler.pojos.RawUrl;
 
+/**
+ * Source for seed URLs
+ *
+ */
 @SuppressWarnings("serial")
-public class SeedUrlSource implements ParallelSourceFunction<RawUrl> {
+public class SeedUrlSource extends RichParallelSourceFunction<RawUrl> {
 
-	private boolean _keepRunning = false;
-	private LinkedList<RawUrl> _urls;
+	private RawUrl[] _unpartitioned;
+	
+	private transient boolean _keepRunning = false;
+	private transient LinkedList<RawUrl> _urls;
 	
 	public SeedUrlSource(float estimatedScore, String... rawUrls) {
-		_urls = new LinkedList<>();
-		for (String rawUrl : rawUrls) {
-			_urls.push(new RawUrl(rawUrl, estimatedScore));
+		_unpartitioned = new RawUrl[rawUrls.length];
+		
+		for (int i = 0; i < rawUrls.length; i++) {
+			_unpartitioned[i] = new RawUrl(rawUrls[i], estimatedScore);
 		}
 	}
 
 	public SeedUrlSource(RawUrl... rawUrls) {
-		_urls = new LinkedList<>();
-		_urls.addAll(Arrays.asList(rawUrls));
+		_unpartitioned = rawUrls;
 	}
 
+	@Override
+	public void open(Configuration parameters) throws Exception {
+		super.open(parameters);
+		
+		_urls = new LinkedList<>();
+		
+		// Figure out which URLs in the full (unpartitioned) set are ones that
+		// this source task should be emitting.
+		RuntimeContext context = getRuntimeContext();
+		int parallelism = context.getNumberOfParallelSubtasks();
+		int myIndex = context.getIndexOfThisSubtask();
+		
+		for (int i = 0; i < _unpartitioned.length; i++) {
+			if ((i % parallelism) == myIndex) {
+				_urls.add(_unpartitioned[i]);
+			}
+		}
+	}
+	
 	@Override
 	public void cancel() {
 		_keepRunning = false;
