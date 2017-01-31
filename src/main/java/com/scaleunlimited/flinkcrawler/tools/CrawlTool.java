3cbb2dda6eae9d81dd80cdd23be16ed9a10682d7
Chris Schneider <Schmed@TransPac.com>
2017-01-30 17:55:12 -0800

    Support a -singleDomain option in CrawlTool

--- src/main/java/com/scaleunlimited/flinkcrawler/tools/CrawlTool.java
+++ src/main/java/com/scaleunlimited/flinkcrawler/tools/CrawlTool.java
@@ -20,24 +20,59 @@ import com.scaleunlimited.flinkcrawler.tools.CrawlTopology.CrawlTopologyBuilder;
 import com.scaleunlimited.flinkcrawler.urls.SimpleUrlLengthener;
 import com.scaleunlimited.flinkcrawler.urls.SimpleUrlNormalizer;
 import com.scaleunlimited.flinkcrawler.urls.SimpleUrlValidator;
+import com.scaleunlimited.flinkcrawler.utils.DomainNames;
 
-import crawlercommons.robots.SimpleRobotRulesParser;
 import crawlercommons.fetcher.http.SimpleHttpFetcher;
 import crawlercommons.fetcher.http.UserAgent;
+import crawlercommons.robots.SimpleRobotRulesParser;
 
 public class CrawlTool {
 
 	static class CrawlToolOptions {
 	    private String _urlsFilename;
+	    private String _singleDomain;
 
 		@Option(name = "-seedurls", usage = "text file containing list of seed urls", required = true)
 	    public void setSeedUrlsFilename(String urlsFilename) {
 	        _urlsFilename = urlsFilename;
 	    }
-	
+		
+		@Option(name = "-singledomain", usage = "only fetch URLs within this domain (and its sub-domains)", required = false)
+	    public void setSingleDomain(String urlsFilename) {
+			_singleDomain = urlsFilename;
+	    }
+		
+		
 		public String getSeedUrlsFilename() {
 			return _urlsFilename;
 		}
+		
+		public boolean isSingleDomain() {
+			return (_singleDomain != null);
+		}
+		
+		public String getSingleDomain() {
+			return _singleDomain;
+		}
+	}
+	
+	@SuppressWarnings("serial")
+	static class SingleDomainUrlValidator extends SimpleUrlValidator {
+	    private String _singleDomain;
+	    
+		public SingleDomainUrlValidator(String singleDomain) {
+			super();
+			_singleDomain = singleDomain;
+		}
+
+		@Override
+		public boolean isValid(String urlString) {
+			if (!(super.isValid(urlString))) {
+				return false;
+			}
+			return DomainNames.isUrlWithinDomain(urlString, _singleDomain);
+		}
+		
 	}
 
 
@@ -72,6 +107,10 @@ public class CrawlTool {
 			StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
 			
 			
+			SimpleUrlValidator urlValidator =
+				(	options.isSingleDomain() ?
+					new SingleDomainUrlValidator(options.getSingleDomain())
+				:	new SimpleUrlValidator());
 			CrawlTopologyBuilder builder = new CrawlTopologyBuilder(env)
 				.setUrlSource(createSeedUrlSource(seedUrlsFile))
 				.setCrawlDB(new InMemoryCrawlDB())
@@ -81,7 +120,7 @@ public class CrawlTool {
 				.setPageParser(new SimplePageParser())
 				.setContentSink(new DiscardingSink<ParsedUrl>())
 				.setUrlNormalizer(new SimpleUrlNormalizer())
-				.setUrlFilter(new SimpleUrlValidator())
+				.setUrlFilter(urlValidator)
 				.setPageFetcher(new SimpleHttpFetcher(new UserAgent("bogus", "bogus@domain.com", "http://domain.com")));
 			
 			builder.build().execute();
